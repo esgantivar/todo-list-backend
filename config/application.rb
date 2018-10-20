f9a97b3b97cf1329dba9264466033480b3676780
Sneyder Gantiva <esgantivar@unal.edu.co>
2018-10-20 02:05:23 -0500

    ajuste cors

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module Todo
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
+        resource '*', headers: :any, methods: [:get, :post, :options, :patch, :destroy]
       end
     end
 
